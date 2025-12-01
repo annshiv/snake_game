e62dbe1fa19046a5800a8d5da1d62d9659e4cec3
Annshiv <annamalaipalani11@gmail.com>
2025-12-01 15:36:54 +0530

    wasm fetch

---
+++ www/index.js
@@ -0,0 +1,13 @@
+async function init() {
+  const response = await fetch("sum.wasm");
+  const buffer = await response.arrayBuffer();
+
+  const wasm = await WebAssembly.instantiate(buffer);
+  const sumFunction = wasm.instance.exports.sum;
+
+  const result = sumFunction(10, 50);
+
+  console.log(result);
+}
+
+init();
