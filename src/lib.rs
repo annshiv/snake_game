d84dba0b9accb509161301c178b88aecf6715316
Annshiv <annamalaipalani11@gmail.com>
2025-12-04 15:20:46 +0530

    draw sbake and world

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,38 @@ static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub struct World {
-    pub width: usize,
+    width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
-        World { width: 8 }
+        World {
+            width: 8,
+            snake: Snake::new(10),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_index_head(&self) -> usize {
+        self.snake.body[0].0
+    }
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,5 +1,44 @@
 import init, { World } from "snake_game";
 
 init().then((_) => {
-  console.log(World.new().width);
+  const world = World.new();
+  const canvas = document.getElementById("snake-canvas");
+  const ctx = canvas.getContext("2d");
+
+  const worldSize = world.width();
+  const CELL_SIZE = 20;
+
+  canvas.width = worldSize * CELL_SIZE;
+  canvas.height = worldSize * CELL_SIZE;
+
+  const drawWorld = () => {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldSize + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldSize * CELL_SIZE);
+    }
+
+    for (let y = 0; y < worldSize + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldSize * CELL_SIZE, CELL_SIZE * y);
+    }
+
+    ctx.stroke();
+  };
+
+  const snakeIdx = world.snake_index_head();
+  const col = snakeIdx % worldSize;
+  const row = Math.floor(snakeIdx / worldSize);
+
+  const drawSnake = () => {
+    ctx.beginPath();
+
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+    ctx.stroke();
+  };
+
+  drawWorld();
+  drawSnake();
 });
